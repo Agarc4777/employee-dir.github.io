e76c54a619a38c8199aeceae88dbef6c781aa04f
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-03-26 16:22:18 -0500

    added states to my app.js

--- employee-dir/src/App.js
+++ employee-dir/src/App.js
@@ -7,16 +7,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(
     () => {
       randomuser.Employee().then(res => {
         setUsers(res.data.results)
       })
     }, [])
+    const handleInputChange = (e) => {
+      const {name,value} = e.target
+      console.log(name, value)
+      setSearch(value);
+    }
   return (
     <div className="App">
       <div class="ui input">
-        <input type="text" placeholder="Search..."/>
+        <input type="text" placeholder="Search..." onChange={handleInputChange} name="search"/>
+        sear
       </div>
       <Results
       users={users}
